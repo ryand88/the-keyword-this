b791e4de41b7fb476028a68ff55e6273d6a15129
ryand88 <ryand8@hotmail.com>
2017-05-18 18:39:26 -0600

    done

--- practice.js
+++ practice.js
@@ -24,16 +24,34 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+    var user = {
+      username: "Ryan",
+      email: "ryan@email.com",
+      getUsername: function(){
+        return user.username;
+      }
+    }
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
-
+user.getUsername();
 //Next Problem
 
 
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
   //Function Invocations Here
+
+  function Car(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    var move = 0;
+    this.moveCar = function(){
+      move++;
+      return move;
+    }
+  }
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -51,12 +69,15 @@ var getYear = function(){
   return this.year;
 };
 
+
 //Above you're given the getYear function. Call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
 //Note(no tests)
   //Code Here
-
-
+mustangYear = getYear.bind(mustang);
+console.log(mustangYear());
+priusYear = getYear.bind(prius);
+console.log(priusYear());
 //New Problem
 
 var myUser = {
@@ -69,15 +90,16 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.bind(myUser); //Fix this
+console.log(userName());
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
-  //Answer Here
+  //Answer Here undefined
 
 //In the example above, what is the 'this keyword' bound to when getMyUsername runs?
 
-  //Answer Here
+  //Answer Here myUser
 
 
 //Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
